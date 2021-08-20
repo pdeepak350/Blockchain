3ce7ef33d6b4c0557c8c55780f267833a8e9c37f
Deepak <plox@localhost.localdomain>
2021-08-20 19:51:17 +0530

    Updated Contract

--- Student.sol
+++ Student.sol
@@ -12,6 +12,22 @@ contract StudentTest {
     function setDetails(string memory _st_name, string memory _reg_number, string memory _st_gender, uint _enrol_year) public{
         s1 = Student(_st_name, _reg_number, _st_gender, _enrol_year);
     }
+    
+    function setName(string memory _st_name) public {
+        s1.st_name=_st_name;
+    }
+    
+    function setRegNo(string memory _reg_number) public {
+        s1.reg_number=_reg_number;
+    }
+    
+    function setGender(string memory _st_gender) public {
+        s1.st_gender=_st_gender;
+    }
+    
+    function setEnrollment(uint  _enrol_year) public {
+        s1.enrol_year=_enrol_year;
+    }
 
      function getDetails() public view returns(string memory, string memory,string memory,uint)  {
         return (s1.st_name, s1.reg_number, s1.st_gender, s1.enrol_year );
